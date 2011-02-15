f59830b7427643087507819c699dc4be154162f3
jaymz <jaymz@u-dox.com>
2011-02-15 13:54:30 +0000

    add in setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='django-ooyala',
+      version='0.9.1',
+      description='A django library for interacting with the Ooyala video platform',
+      author='Jaymz Campbell',
+      author_email='jaymz@jaymz.eu',
+      url='https://github.com/jaymzcd/django-ooyala',
+      packages=['ooyala',],
+     )
