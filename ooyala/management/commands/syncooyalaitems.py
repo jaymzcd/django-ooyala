0957637e6d2f8499ddf4dc743fe723937e74eb44
Tom Viner <tom.viner@u-dox.com>
2011-06-28 12:47:31 +0100

    fix notification newlines

--- ooyala/management/commands/syncooyalaitems.py
+++ ooyala/management/commands/syncooyalaitems.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
     help = 'Imports all channel and video content from Ooyala to the admin'
 
     def handle(self, *args, **kwargs):
+        VIDEOS_PER_PAGE = 500
         count = 0
         offset = 1
         retries_left = 5
@@ -18,11 +19,11 @@ class Command(BaseCommand):
 
             if not isinstance(ooyala_response, basestring):
                 items = ooyala_response.getElementsByTagName('item')
-                if not items:
-                    sys.stdout.write('\nNo more items after offset %d\n' % offset)
-                    break
+                if items:
+                    sys.stdout.write('Found items %d - %d\n' % (offset, offset+len(items)-1))
                 else:
-                    sys.stdout.write('\nFound %d items from offset %s\n' % (len(items), offset))
+                    #No more items
+                    break
                 for item in items:
                     [ooyala_item, created] = OoyalaItem.from_xml(item)
                     try:
@@ -30,18 +31,23 @@ class Command(BaseCommand):
                             count += 1
                             sys.stdout.write('Added %s (%s)\n' % (str(ooyala_item.title), ooyala_item.content_type))
                         else:
-                            sys.stdout.write("Skipping %s" % str(ooyala_item.title))
+                            sys.stdout.write("Skipping %s\n" % str(ooyala_item.title))
+                            pass
                     except UnicodeEncodeError:
-                        sys.stdout.write('decode error for title')
+                        sys.stdout.write('Decode error for title\n')
+                if len(items) < VIDEOS_PER_PAGE:
+                    #This was the last page of items
+                    break
             else:
                 sys.stdout.write('Problem getting the data from ooyala, retrying...\n')
                 retries_left -= 1
                 if retries_left <= 0:
-                    sys.stdout.write('TOO MANY RETRIES GIVING UP!')
+                    sys.stdout.write('TOO MANY RETRIES GIVING UP!\n')
                     break
                 continue # keep same offset
 
-            offset += 500
+            offset += VIDEOS_PER_PAGE
+
 
         sys.stdout.write('\nCOMPLETE: All items imported - %s in total\n\n' % count)
 
