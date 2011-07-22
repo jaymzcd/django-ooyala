8ace0ff89c5ca058470e305974c92ac47becb469
jaymz <jaymz@u-dox.com>
2011-07-22 19:11:12 +0800

    update setup.py for new release

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(name='django-ooyala',
-      version='0.9.1',
+      version='0.9.3',
       description='A django library for interacting with the Ooyala video platform',
       author='Jaymz Campbell',
       author_email='jaymz@jaymz.eu',
       url='https://github.com/jaymzcd/django-ooyala',
-      packages=['ooyala',],
+      packages=['ooyala', 'ooyala.templatetags', 'ooyala.templates', 'ooyala.management'],
      )
